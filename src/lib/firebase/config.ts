f3a274177c31ea4d21e899e033b760b73cf8e889
PREM HAGARAGI <premversace@gmail.com>
2025-07-07 17:02:52 +0000

    blog post, skelrton loader, fetch blog and mapping

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,6 +3,7 @@ import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 // firebase config
+
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
